afbaf40b17fd1747c68d36c3e5de046b3e8aa252
hermesdev <hermesdev0131@gmail.com>
2025-01-23 10:19:39 -0600

    Make port dynamic

--- server.py
+++ server.py
@@ -69,4 +69,5 @@ def handle_post():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
